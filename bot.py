6f9fc099b06496ed75946584c57b8fbceb67fab6
Liz Miller <liz.miller11@gmail.com>
2022-11-18 21:58:35 -0500

    write neopixel index to arduino via serial

--- bot.py
+++ bot.py
@@ -1,21 +1,34 @@
 import serial
 import time
 
-arduino = serial.Serial(port = 'COM3', timeout=0)
+arduino = serial.Serial(port = "/dev/cu.usbmodem14201", timeout=0)
 time.sleep(2)
 
-while True:
+# get the position, encode it, write to arduino
+setpos = 3
+setpos = str(setpos)
+arduino.write(setpos.encode())
+#time.sleep(1)
 
-    print ("Enter '1' to turn 'on' the LED and '0' to turn LED 'off'")
 
-    var = str(input())
-    print ("You Entered :", var)
+# while True:
+#
+#     #manually enter
+#     print ("Which NeoPixel Position to light?")
+#
+#     pos = input()
+#     print ("You Entered:", pos)
+#
+#     arduino.write(pos.encode())
 
-    if(var == '1'):
-        arduino.write(str.encode('1'))
-        print("LED turned on")
-        time.sleep(1)
-
-    if(var == '0'):
-        arduino.write(str.encode('0'))
-        print("LED turned off")
+    # var = str(input())
+    # print ("You Entered :", var)
+    #
+    # if(var == '1'):
+    #     arduino.write(str.encode('1'))
+    #     print("LED turned on")
+    #     time.sleep(1)
+    #
+    # if(var == '0'):
+    #     arduino.write(str.encode('0'))
+    #     print("LED turned off")
